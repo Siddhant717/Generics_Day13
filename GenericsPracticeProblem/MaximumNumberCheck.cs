858f622769491326b8598f230c6174aa77d2f6aa
Siddhant <santoshshrivastava39@gmail.com>
2022-06-26 13:48:12 +0530

    initial commit

---
+++ GenericsPracticeProblem/MaximumNumberCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPracticeProblem
+{
+    public class MaximumNumberCheck
+    {
+        /// <summary>
+        /// This method is created for max int number
+        /// </summary>
+            public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;                                                                         
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstnumber,secondnumber,thirdnumber are same");
+
+        }
+    }
+}
---
+++ GenericsPracticeProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum number");
+            int answer = MaximumNumberCheck.MaximumIntegerNumber(223, 323, 443);                    
+            Console.WriteLine(answer);
+        }
+    }
+}
