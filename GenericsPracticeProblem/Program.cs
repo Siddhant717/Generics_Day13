33348f891bb7c4a7fc038f8eeb43a87f8d4a03b1
Siddhant <santoshshrivastava39@gmail.com>
2022-06-26 15:53:16 +0530

    UC5 solution added

--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -6,20 +6,11 @@ namespace GenericsPracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Integer Number- ");
-            int[] intArray = { 120, 140, 200, 500 };
+           
             GenericsMaximum<int> generic = new GenericsMaximum<int>(intArray);
             generic.PrintMaxValue();
 
-            Console.WriteLine("Float Number- ");
-            double[] doubleArray = { 10.34, 15.65, 2.34, 12.22 };
-            GenericsMaximum<double> genericDouble = new GenericsMaximum<double>(doubleArray);
-            genericDouble.PrintMaxValue();
-
-            Console.WriteLine("String- ");
-            string[] stringArray = { "15", "65", "30", "10" };
-            GenericsMaximum<string> genericString = new GenericsMaximum<string>(stringArray);
-            genericString.PrintMaxValue();
+            
         }
     }
 }
